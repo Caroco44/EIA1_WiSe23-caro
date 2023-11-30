f1369602f93b625f2cdf4b3939d69c147c28c38c
Caroco44 <caroline04kraemer@yahoo.de>
2023-11-30 16:40:20 +0100

    L05_231130

---
+++ L05/script.js
@@ -0,0 +1,21 @@
+let text1 = prompt("hello ur computer has virus, u want me to help? yes or no")
+
+if (text1 == "yes") {
+    let text2 = prompt("Ok, first, you agree to send me 500dollars? Yes or no")
+
+if (text2 == "yes") {
+    let text2 = prompt("Ok thank you, ur computer has been restored, the money has been send.")
+} else {
+    let text2 = prompt("well then, got no choice, I gotta hack ur bank account")
+}
+
+
+} else if (text1 == "no") {
+    let text2 = prompt("What do you mean no. For being rude u owe me 500dollars. Will u give it to me? Yes or no");
+
+    if (text2 == "yes") {
+        let text2 = prompt("Thank you very much, u are nice after all. I got the money and you got good compi")
+    } else {
+        let text2 = prompt("Ok fine, ill just go hack someone else")
+    }
+}
